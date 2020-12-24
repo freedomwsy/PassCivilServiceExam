4a9e584e821e5aa99f2ef862e50b65bb1bf0d513
freedomwsy <757951671@qq.com>
2020-12-24 15:09:08 +0800

    创建基础工程

---
+++ pass-cse-content/src/test/java/com/wsy/content/ContentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsy.content;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
